4d794a960417546100e08c905606ad7c890881c6
miromiroslavov <miroslav.miroslavov@gmail.com>
2014-09-10 07:02:42 +0300

    Styling and navigation track.

--- assets/javascripts/main.js
+++ assets/javascripts/main.js
@@ -1,1 +1,8 @@
-console.log('This would be the main JS file.');
+$(function () {
+    var pathArray = window.location.pathname.split('/'),
+        loc = pathArray[1],
+        list = $('.list-group');
+
+    list.find('.active').removeClass('active');
+    list.find('[href=/' + loc + ']').addClass('active');
+});
